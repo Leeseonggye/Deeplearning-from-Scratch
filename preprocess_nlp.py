f43a7f3d6835ab7e292896e533d1e9e467bebfca
Tony <55056131+Leeseonggye@users.noreply.github.com>
2020-12-06 22:57:33 +0900

    Add files via upload

---
+++ preprocess_nlp.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[7]:
+
+
+import numpy as np
+
+def preprocess(text):
+    text = text.lower()
+    text = text.replace('.',' .')
+    words = text.split(' ')
+    
+    word_to_id = {}
+    id_to_word = {}
+    
+    for word in words:
+        if word not in word_to_id:
+            new_id = len(word_to_id)
+            word_to_id[word] = new_id
+            id_to_word[new_id] = word
+    
+    corpus = np.array([word_to_id[w] for w in words])
+    
+    return corpus, word_to_id,id_to_word
+
+
+# In[8]:
+
+
+preprocess('I need you')
+
